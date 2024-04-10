e527e3acbe84075d9588a72818775e55bef1530f
Typhoon1217 <jungwooshin.97@gmail.com>
2024-04-10 19:28:14 +0900

    update: 서버로 구현, 몇가지 문제점 수정
    -마지막 커밋 잘못올림(fix:LastCommit)
    -로그인 인터페이스-->추상 클래스로 변경 로그인 메소드 추가
    -로그인 실패시 카운트 기능 추가 (클라이언트 정규식 오류 카운트 하지 않기 위해서 정규식 부분에 걸지 않고 클-라이언트에서 이상한짓을 할것을 염두해서 서버에서 카운트)
    -로그인 정보 파일로 관리
    -클라이언트 로그인 기능 메소드로 변경
    -싱글톤 포기(동시에 다양한 로그인 처리를 위해 쓰레드 사용
    -다른 폴더로 관리하기 위해 서버 프로젝트 새로 생성
    -패키지명 소문자로 수정

--- Kh_Project1_Restaurant/src/main/RestaurantMain.java
+++ Kh_Project1_Restaurant/src/main/RestaurantMain.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
---
+++ Kh_Project1_RestaurantServer/src/login/Admin.java
@@ -0,0 +1,14 @@
+package login;
+
+
+//관리자 클래스
+public class Admin extends User {
+ public Admin(String id, String password) {
+     super(id, password);
+ }
+
+ @Override
+ public boolean LogIn(String id, String password) {
+     return this.id.equals(id) && this.password.equals(password);
+ }
+}
