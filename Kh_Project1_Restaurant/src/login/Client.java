3829b81da38f1a1792857ca9009b4026bb1d1d76
Typhoon1217 <jungwooshin.97@gmail.com>
2024-04-10 13:26:23 +0900

    test: 서버로그인 기능 테스트코드 추가
    -서버 로그인 기능 테스트
    
    추후 작업내용
    -관리자 로그인 일반 사용자 로그인 구현
    -아이디 입력 조회 정규식 구현(서버와 클라이언트 모두 적용)
    -관리자 추가 삭제
    -관리자 비밀번호 수정
    -직원 추가 삭제
    -직원 비밀번호 수정
    -관리자 비밀번호 수정
    
    -메뉴판 관리기능 추가

--- Kh_Project1_Restaurant/src/login/Client.java
+++ Kh_Project1_Restaurant/src/login/Client.java
@@ -1,4 +1,4 @@
-package testClient;
+package login;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -6,21 +6,21 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class TestClient {
-	private static final String SERVER_IP = "localhost";  // 서버 IP 주소
+public class Client {
+	private static final String SERVER_IP = "::1";  				// 서버 IP 주소
 	public static void main(String[] args) {
 		try {
-			Socket s = new Socket(SERVER_IP, 6666);
+			Socket s = new Socket(SERVER_IP, 1234);
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("아이디를 입력하세요: ");
+			System.out.print("ID: ");
 			String id = reader.readLine();
-			System.out.print("비밀번호를 입력하세요: ");
+			System.out.print("PW: ");
 			String password = reader.readLine();
 
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-			dos.writeUTF(id);  // 아이디 입력
-			dos.writeUTF(password);  // 비밀번호 입력
+			dos.writeUTF(id);											
+			dos.writeUTF(password);										
 
 			DataInputStream dis = new DataInputStream(s.getInputStream());
 			String response = dis.readUTF();
